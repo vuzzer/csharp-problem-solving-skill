386788a1ab11bb8d0f5784819c2666648510ce8e
parfait kouassi <parfaitkouess@gmail.com>
2024-04-29 22:01:20 -0400

    solution to palindrome and trapping rain water

---
+++ Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+static bool Solution(String s)
+{
+    s = s.ToLower();
+    string messageSanitize = Regex.Replace(s, "[^a-zA-Z0-9]", "");
+    int halfSize = messageSanitize.Length / 2;
+    if(halfSize > 0)
+    {
+        for (int right = 0, left = messageSanitize.Length - 1; right <= halfSize; right++, left--)
+        {
+            if (messageSanitize[right] != messageSanitize[left])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+string message = "A man, a plan, a canal: Panama";
+
+// Display message sanitize
+Console.WriteLine(Solution(message));
+
+
+
---
+++ TrappingRainWater/Program.cs
@@ -0,0 +1,27 @@
+﻿static int Trap(int[] heights)
+{
+    int left = 0, right = heights.Length - 1;
+    int maxRight = heights[right], maxLeft = heights[left];
+    int result = 0;
+    while(right > left)
+    {
+       if(maxLeft < maxRight)
+        {
+            left += 1;
+            maxLeft = int.Max(maxLeft, heights[left]);
+            result += maxLeft - heights[left];
+        }
+        else
+        {
+            right -= 1;
+            maxRight = int.Max(maxRight, heights[right]);
+            result += maxRight - heights[left];
+        }
+    }
+    return result;
+}
+
+
+int[] entree = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+Trap(entree);
